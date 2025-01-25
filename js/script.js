573688acbd8f6db26392901b91e9a4d7e6700025
Ashot <harutyunyanashot0497@mail.ru>
2025-01-25 23:53:02 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -6,35 +6,23 @@ const hidden = document.getElementById("hidden")
 
 const triggers = new Set(["h1", "span", "p"])
 
-constructor.addEventListener("dragstart", e => {
-	if(e.target === e.currentTarget) return
-
-	e.target.classList.add("dragging")
-	e.dataTransfer.setData("text/plain", e.target.dataset.id)
-})
-
-target.addEventListener("dragover", e => e.preventDefault())
-
-target.addEventListener("drop", e => {
-
-	if(e.target.nodeName !== "DIV") return
-	const id = e.dataTransfer.getData("text/plain")
-	const droppable = document.querySelector(`[data-id="${id}"]`)
-
-	e.target.appendChild(droppable)
-	droppable.classList.remove("dragging")
-	droppable.draggable = false
-})
-
 function createElem(tag, content){
 	const newElement = document.createElement(tag)
 	newElement.classList.add("created")
 	newElement.draggable = true
 	newElement.dataset.id = crypto.randomUUID()
 	if(tag === "input") newElement.placeholder = 'input teg'
-	if(content) newElement.innerHTML = `<pre>&#60;${tag}&#62;</pre> ${content}`
-	else newElement.innerHTML = `<pre>&#60;${tag}&#62;</pre>`
 
+	const pre = document.createElement('pre')
+	pre.textContent = `<${tag}>`
+
+	if(content) {
+		newElement.textContent = content
+		newElement.appendChild(pre)
+	}
+
+	newElement.appendChild(pre)
+ 
 	return newElement
 }
 
@@ -54,4 +42,24 @@ type.addEventListener('change', function(){
 create.addEventListener('click', () => {
 	const newElem = createElem(type.value, hidden.value)
 	pushElement(newElem, constructor)
+})
+
+constructor.addEventListener("dragstart", e => {
+	if(e.target === e.currentTarget) return
+
+	e.target.classList.add("dragging")
+	e.dataTransfer.setData("text/plain", e.target.dataset.id)	
+})
+
+target.addEventListener("dragover", e => e.preventDefault())
+
+target.addEventListener("drop", e => {
+
+	if(e.target.nodeName !== "DIV") return
+	const id = e.dataTransfer.getData("text/plain")
+	const droppable = document.querySelector(`[data-id="${id}"]`)
+
+	e.target.appendChild(droppable)
+	droppable.classList.remove("dragging")
+	droppable.draggable = false
 })
